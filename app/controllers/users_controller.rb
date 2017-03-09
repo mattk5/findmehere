f5d278723eacda24b2abdb49c2263dd6af54680d
mattk5 <mattkubota5@gmail.com>
2017-03-08 21:31:46 -0500

    Created functionality to edit user profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,  only: [:edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -15,6 +18,20 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user 
+    else
+      render 'edit'
+    end
+  end 
+
   def show
   	@user = User.find(params[:id])
   end
@@ -24,4 +41,15 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Sign in to update profile."
+        redirect_to login_url
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
 
 	def current_user
 		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
 			if user && user.authenticated?(cookies[:remember_token])
@@ -13,6 +13,10 @@ module SessionsHelper
 				@current_user = user 
 			end
 		end
+	end
+
+	def current_user?(user)
+		user == current_user
 	end
 
 	def logged_in?
